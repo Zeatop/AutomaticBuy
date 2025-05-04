a59ac5ee8e337cf2fa8de550adb97e0895b05237
léo jackson <leo_jackson@hotmail.fr>
2025-05-04 11:29:43 +0200

    Fix KingJouet/config.py

--- websites/KingJouet/config.py
+++ websites/KingJouet/config.py
@@ -38,18 +38,21 @@ SELECTORS = {
     
     
     # Panier
-    "cart_items": "#header-cart-infos",
+    # On pourrait ne regarder que les cart_item plutôt que de s'embêter à séparer les préco et non préco
+    "cart_items_available": ".relative divide-y", # Le premier sont les produits disponibles
+    "cart_items_preco": ".relative divide-y", # Le deuxième sont les produits en précommande
+    "cart_item": "panier-article-row border-b",
     # On ne peut modifier et voir la quantité que dans le panier
     "quantity_input": ".px-2",
-    # C'est la même classe pour le bouton ajouter ou retirer une quantité d'un item
-    "add_remove_item": "btn btn-circle p-4 bg-neutral-200 border-none font-semibold btn-process",
+    "remove_item": "btn btn-circle p-4 bg-neutral-200 border-none font-semibold btn-process", # Le premier sert à remove un item
+    "add_remove_item": "btn btn-circle p-4 bg-neutral-200 border-none font-semibold btn-process", # Le second sert à ajouter un item
     "cart_total": ".attribut-value.text-right.block.font-bold.text-lg.md:text-xl",
-    "proceed_to_checkout": "#btn_confirmation",
+    "proceed_to_checkout": "#btn_confirmation_pc",
     # Impossible de vider le panier en un clic
-    "empty_cart": ".cart-empty",
+    "empty_cart": ".text-sm.underline.cursor-pointer",
     
     # Connexion
-    "account-button": ".account-wrapper",
+    "account-button": ".kj-icon-compte1.text-3xl.lg:text-4xl",
     "email_input": "#login-email-input",
     "password_input": "#login-password-input",
     "login_button": ".btn.btn-orange.btn-process.w-1/2.ml-2",
